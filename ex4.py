3db52221c494db1a53403a11b6c32694c4c3a271
ereminmax <eremin.max@gmail.com>
2017-04-12 04:59:00 -0700

    fourth task

---
+++ ex4.py
@@ -0,0 +1,10 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven =cars - drivers
+
+print "There are", cars, "cars available"
+print "There are", drivers, "drivers available"
+print "There will be", cars_not_driven, "empty cars today"
+print "I%slove"%"you"
