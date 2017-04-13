2897b9a38758614adb337e3d73fad4e24b9e5257
ereminmax <eremin.max@gmail.com>
2017-04-13 11:44:48 +0400

    fifth

---
+++ ex5.py
@@ -0,0 +1,6 @@
+my_name = 'Max EREMIN'
+my_height = 183
+my_weight = 60
+
+print "Let's talk about %s" % my_name
+print "He's%d height and%d kg weight To sum up: %d"  % (my_height, my_weight, my_height+my_weight)
