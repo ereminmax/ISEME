ba3f053245f3f946d464e2db78e206602d7f5541
ereminmax <eremin.max@gmail.com>
2017-04-14 18:52:40 +0400

    21

---
+++ ex21.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def div(a,b):
+    return a*1.0/b
+age=add(20,2)
+height = sub(190,4)
+weight =mult(30,2)
+iq=div(1000,2)
+print (age,height,weight,iq)
