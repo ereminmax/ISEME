ab12a5ca48f296f32504db4b5d7346496323d88f
ereminmax <eremin.max@gmail.com>
2017-04-14 17:41:28 +0400

    18

---
+++ ex19.py
@@ -0,0 +1,9 @@
+def cheese_crackers(ch_amount, boxes_amount):
+    print "You have %d cheese!"%ch_amount
+    print "You have %d boxes of crackers"%boxes_amount
+cheese_crackers(100, 30)
+amch=10
+amcr=20
+cheese_crackers(amch,amcr)
+cheese_crackers(5+2, 4+3)
+cheese_crackers(amch+4, amcr+5)
