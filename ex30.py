22f365a08b2f5ab6d14181122e41d1f93fed7902
ereminmax <eremin.max@gmail.com>
2017-04-17 18:19:59 +0400

    30

---
+++ ex30.py
@@ -0,0 +1,22 @@
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print "Too many cats!"
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can\'t decide!"
+
+if trucks > cars:
+    print "People are greater"
+elif trucks < cars:
+    print "People are less"
+else:
+    print "People are dogs."
+
+if people > trucks:
+    print "Alright"
+else:
+    print "Fine"
