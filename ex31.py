8594b164b4320b93191b0e38b31986f5291179dd
ereminmax <eremin.max@gmail.com>
2017-04-17 18:30:12 +0400

    31

---
+++ ex31.py
@@ -0,0 +1,23 @@
+door =raw_input("> ")
+if door=="1":
+    print "one"
+
+    bear =raw_input("> ")
+
+    if bear == "1":
+        print "One"
+    elif bear == "2":
+        print "Two"
+    else:
+        print "Else"
+elif door =="2":
+    print "two"
+
+    insanity = raw_input("> ")
+
+    if insanity == "1" or insanity == "2":
+        print "One or two"
+    else:
+        print "Else"
+else:
+    print "else"
