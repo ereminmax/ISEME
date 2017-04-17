e924b212053ba5a6b660119188fcc629ff7cb115
ereminmax <eremin.max@gmail.com>
2017-04-17 18:05:44 +0400

    29

---
+++ ex29.py
@@ -0,0 +1,23 @@
+people = 20
+cats = 30
+dogs =15
+
+if people < cats:
+    print "Too many cats!"
+
+if people > cats:
+    print "Not many cats!"
+
+if people < dogs:
+    print "The world is dry!"
+
+dogs += 5
+
+if people >= dogs:
+    print "People are greater"
+
+if people <= dogs:
+    print "People are less"
+
+if people == dogs:
+    print "People are dogs."
