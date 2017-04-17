8a9add303c8015480e6f2996e8ef39b7e2373eb0
ereminmax <eremin.max@gmail.com>
2017-04-17 19:16:49 +0400

    34

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+print """The animal at 1 is %s"
+The third animal is %s
+The first animal is %s
+The animal at 3 is %s
+The fifth animal %s
+The animal at 2 is %s
+The sixth animal is %s
+The animal at 4 is %s
+""" % (animals[1], animals[2], animals[0], animals[3], animals[4], animals[2], animals[5], animals[4])
