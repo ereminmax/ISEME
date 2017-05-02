49b3cba83613b64ace1cacaca4c82954e3cd7c13
ereminmax <eremin.max@gmail.com>
2017-05-02 19:46:31 +0400

    Added a custom postadmin in order to improve the UX

--- netmag/blog/admin.py
+++ netmag/blog/admin.py
@@ -5,4 +5,13 @@ from django.contrib import admin
 
 # Register your models here.
 from blog.models import Post
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    list_filter = ['published', 'created']
+    search_fields = ['title', 'description', 'content']
+    date_hierarchy = 'created'
+    save_on_top = True
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
