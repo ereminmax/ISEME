4d2ee15cfa56a62a8435850d6a5a271147520c0e
ereminmax <eremin.max@gmail.com>
2017-05-02 20:20:42 +0400

    View functions

--- netmag/blog/views.py
+++ netmag/blog/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.filter(published = True)
+    return render(request, 'blog/index.html', {'posts': posts})
+
+def post(request, slug):
+    post = get_object_or_404(Post, slug = slug)
+    return render(request, 'blog/post.html', {'post': post})
